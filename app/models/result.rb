3f4d16f272364a0119a4095967442e9df98b1e76
Mike DLoss <MikeDLoss@quickenloans.com>
2018-08-16 15:34:55 -0400

    override as_json method to include gifts and exclude ids from json

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,4 +5,9 @@ class Result < ApplicationRecord
   belongs_to :secondary1, :class_name => 'Gift'
   belongs_to :secondary2, :class_name => 'Gift'
   belongs_to :secondary3, :class_name => 'Gift'
+
+  def as_json(options={})
+    super(include: [:primary1, :primary2, :primary3, :secondary1, :secondary2, :secondary3],
+      except: [:primary1_id, :primary2_id, :primary3_id, :secondary1_id, :secondary2_id, :secondary3_id])
+  end
 end
